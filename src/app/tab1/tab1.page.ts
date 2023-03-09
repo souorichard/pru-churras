3028d8d3efc593b67aae7fd0f55d3b2bd70def7a
souorichard <elaine.ra96@gmail.com>
2023-03-09 15:06:39 -0300

    desenvolvendo interface do Tab 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,9 +1,18 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  animations: [
+    trigger('rotatedStated', [
+      state('default', style({transform: 'rotateY(0)'})),
+      state('rotated', style({transform: 'rotateY(-360deg)'})),
+      transition('rotated => default', animate('2000ms ease-out')),
+      transition('default => rotated', animate('2000ms ease-in'))
+    ])
+  ]
 })
 export class Tab1Page {
 
@@ -15,17 +24,25 @@ export class Tab1Page {
 
   info = 'Clique no botão para jogar!';
 
+  state = 'default';
+
   constructor() {}
 
   playCoin() {
-    if (Math.random() < 0.5) {
-      this.image = this.face;
-      this.info = 'Cara!';
-    }
-    else {
-      this.image = this.crown;
-      this.info = 'Coroa!'
-    }
+    this.image = this.logo;
+    this.info = 'Girando...'
+    this.state = this.state === 'default' ? 'rotated' : 'default';
+
+    setTimeout(() => {
+      if (Math.random() < 0.5) {
+        this.image = this.face;
+        this.info = 'Cara!';
+      }
+      else {
+        this.image = this.crown;
+        this.info = 'Coroa!'
+      }
+    }, 2000);
 
     // console.log('Foii');
   }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,45 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  value = 1;
+
+  btn1 = 1;
+  btn2 = 3;
+  btn3 = 6;
+  btn4 = 9;
+  btn5 = 12;
+
+  point = 0;
+  point2 = 0
+
   constructor() {}
+
+  btnAdd1() {
+    this.value = this.btn1;
+  }
+  btnAdd2() {
+    this.value = this.btn2;
+  }
+  btnAdd3() {
+    this.value = this.btn3;
+  }
+  btnAdd4() {
+    this.value = this.btn4;
+  }
+  btnAdd5() {
+    this.value = this.btn5;
+  }
+
+  addValue() {
+    this.point += this.value;
+  }
+
+  removeValue() {
+
+  }
+
+  clean() {
+    console.log("foi")
+  }
 
 }
