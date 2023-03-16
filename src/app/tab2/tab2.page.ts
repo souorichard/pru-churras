f4ed3179048124cf7f205fca752801c352730ae6
souorichard <elaine.ra96@gmail.com>
2023-03-16 09:02:33 -0300

    Finalizando Projeto

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -8,56 +9,78 @@ import { Component } from '@angular/core';
 export class Tab2Page {
 
   value = 1;
-
-  btn1 = 1;
-  btn2 = 3;
-  btn3 = 6;
-  btn4 = 9;
-  btn5 = 12;
-
   point = 0;
-  point2 = 0
+  point2 = 0;
+  round = 0;
+  round2 = 0;
 
-  constructor() {}
+  constructor( private toastController: ToastController ) {}
+
 
   btnAdd1() {
-    this.value = this.btn1;
+    this.value = 1;
   }
 
   btnAdd2() {
-    this.value = this.btn2;
+    this.value = 3;
   }
 
   btnAdd3() {
-    this.value = this.btn3;
+    this.value = 6;
   }
 
   btnAdd4() {
-    this.value = this.btn4;
+    this.value = 9;
   }
 
   btnAdd5() {
-    this.value = this.btn5;
+    this.value = 12;
   }
 
   addValue() {
     this.point += this.value;
+    if (this.point >= 12) {
+      this.round += 1;
+      this.point = 0;
+      this.point2 = 0;
+    }
+
+    this.value = 1;
   }
 
   addValue2() {
     this.point2 += this.value;
+    if (this.point2 >= 12) {
+      this.round2 += 1;
+      this.point = 0;
+      this.point2 = 0;
+    }
+
+    this.value = 1;
   }
 
   removeValue() {
     this.point -= this.value;
+    this.point = this.point < 0 ? 0 : this.point;
+
+    this.value = 1;
   }
 
   removeValue2() {
     this.point2 -= this.value;
+    this.point2 = this.point < 0 ? 0 : this.point;
+
+    this.value = 1;
   }
 
-  clean() {
-    console.log("foi")
+  async cleanToast(position: 'bottom') {
+    const toast = await this.toastController.create({
+      message: 'Cleaned',
+      duration: 1500,
+      position: position
+    });
+
+    await toast.present();
   }
 
 }
